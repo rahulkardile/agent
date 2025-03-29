5972ebaa85e13a3a6bb97165a2cc96daf159e1f6
rahulkardile <rahulkardile321@gmail.com>
2025-03-29 09:11:07 +0530

    added new form

--- app/(auth)/AuthLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react'
-
-const AuthLayout = ({ children } : { children: ReactNode }) => {
-  return (
-    <div>{children}</div>
-  )
-}
-
-export default AuthLayout
---
+++ components/AuthForm.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import Image from "next/image"
+
+const formSchema = z.object({
+    username: z.string().min(2).max(50),
+})
+
+const AuthForm = ({ type } : { type: FormType }) => {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+    }
+    let isSingIn = type === "sign-in";
+    return (
+        <div className="card-border lg:min-w-[566px]">
+            <div className="flex flex-col gap-3 card py-14 px-10 items-center">
+                <div className="flex flex-row gap-4 justify-center">
+                    <Image src="/logo.svg" alt="logo" height={32} width={38} />
+                    <h2 className="text-primary-100">Agent-AI</h2>
+                </div>
+                <h3>Practies Job interview with AI</h3>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-8 mt-4">
+                        {!isSingIn && <p>Name</p>}
+                        <p>Email</p>
+                        <p>Password</p>
+                        <Button type="submit" className="!w-full !bg-primary-200 !text-dark-100 hover:!bg-primary-200/80 !rounded-full !min-h-10 !font-bold !px-5 cursor-pointer">{isSingIn ? "Sign-In" : "Create an Account"}</Button>
+                    </form>
+                </Form>
+            </div>
+        </div>
+    )
+}
+
+export default AuthForm
