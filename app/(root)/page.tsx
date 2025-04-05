e67ef41e62885357487797ca0cab0d1cc548f24a
rahulkardile <rahulkardile321@gmail.com>
2025-04-05 11:17:53 +0530

    fix and demo setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,35 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Home Page</div>
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get interview-ready with AI-Powered Practice & Feedback</h2>
+          <p className='text-lg'>Practice on real interview questions & get instant Feedback</p>
+
+          <Button asChild className='btn-primary max-sm:w-full'><Link href={"/interview"}>Start an Interview</Link></Button>
+        </div>
+        <Image src={"/robot.png"} alt='robo-dude' width={400} height={400} className='max-sm:hidden' />
+      </section>
+
+      <section>
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          <p>You haven&apos;t taken any interview yet</p>
+        </div>
+      </section>
+
+      <section className='flex flex-col gap-6 mt-8'>
+        <h2>Take an Interview</h2>
+        <div className="interviews-section">
+          <p>There are no interview available</p>
+        </div>
+      </section>
+    </>
   )
 }
 
